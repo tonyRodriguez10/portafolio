c031f9f1d5871fec52230ae046ad1341b83f0d9c
AfterLife <anthony.rodriguezmunoz@ucr.ac.cr>
2021-02-27 23:55:41 -0600

    queda listo el armado basico de la pagina 2/27/2021

---
+++ jQuery/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function()
+	    {
+	    $("#soft-skill--btn").on( "click", function() {	 
+	        $(".soft-skills-container").toggle();
+	         });
+		$("#hard-skill--btn").on( "click", function() {	 
+			$(".hard-skills-container").toggle();
+			 });
+	    });
