37ea53a5aa1b05b38b81bba50439b6f755cacce8
AfterLife <anthony.rodriguezmunoz@ucr.ac.cr>
2021-02-28 19:01:57 -0600

    se va entrando en detalles de cada seccion de la pagina 2/28/2021

--- jQuery/script.js
+++ jQuery/script.js
@@ -6,4 +6,7 @@ $(document).ready(function()
 		$("#hard-skill--btn").on( "click", function() {	 
 			$(".hard-skills-container").toggle();
 			 });
+			 $(function(){
+				$('.popup').load('https://es.theysay.me/ranking/111/');
+			  });
 	    });
