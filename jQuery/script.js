bf4cbee3e75ac67c30339b6e172c6601b629c71c
AfterLife <anthony.rodriguezmunoz@ucr.ac.cr>
2021-03-21 18:18:55 -0600

    migracion a version 2 incluyendo nuevas sections y diseños 3/21/2021

--- jQuery/script.js
+++ jQuery/script.js
@@ -16,4 +16,21 @@ $(document).ready(function()
 		$("#portugues--btn").on( "click", function() {	 
 			$(".popup-portugues").toggle();
 			});
+
+			$('nav ul li > a:not(:only-child)').click(function(e) {
+				$(this).siblings('.nav-submenu').toggle();
+				$('.nav-submenu').not($(this).siblings()).hide();
+				e.stopPropagation();
+			  });
+			
+			  $('html').click(function() {
+				$('.nav-submenu').hide();
+			  });
+			  
+			  $('#nav-boton').click(function() {
+				$('nav ul').toggle()
+				$('#nav-boton').toggleClass("activo");
+			  })
+			
+
 	    });
